877be36974688c1ba57619f301d74a59dd8cd354
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-11-24 20:45:07 -0300

    feat: signing-up user to dataBase

--- src/controllers/signUpAuthController.js
+++ src/controllers/signUpAuthController.js
@@ -1,5 +1,22 @@
-import { db } from "../dataBase/db.js"
+import bcrypt from "bcrypt";
+import { dbUsers } from "../dataBase/db.js";
 
-export async function signUpAuthController (req, res) {
-    console.log("entrou aqui!!!")
+export async function signUpAuthController(req, res) {
+  const { name, email, password } = req.body;
+
+  const newPassword = bcrypt.hashSync(password, 10);
+
+  const user = {
+    name,
+    email,
+    password: newPassword,
+  };
+
+  try {
+    await dbUsers.insertOne(user);
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    es.sendStatus(400);
+  }
 }
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,16 +1,15 @@
 import { authSchema } from "../models/authSchema.js";
 
 export async function authMiddleware(req, res, next) {
-  const { name, email, password } = req.body;
-  const signUp = {
-    name,
-    email,
-    password,
-  };
-
   try {
+    const { name, email, password } = req.body;
+    const signUp = {
+      name,
+      email,
+      password,
+    };
     await authSchema.validateAsync(signUp, { abortEarly: false });
-    res.sendStatus(200);
+    
     next();
   } catch (err) {
     res.status(401).send(err.details.map((d) => d.message));
