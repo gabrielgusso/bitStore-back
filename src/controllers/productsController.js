22c29126fe56f6fa6bb59ce17f0d6983529a25cd
gabrielgusso <gabriel.gusso@hotmail.com>
2022-11-24 14:27:24 -0300

    feat: products route

---
+++ src/controllers/productsController.js
@@ -0,0 +1,5 @@
+import { db } from "../dataBase/db.js"
+
+export async function productsController (req, res) {
+    console.log("entrou")
+}
---
+++ src/routes/productsRoute.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { productsController } from "../controllers/productsController.js";
+
+export const productsRoute = Router();
+
+productsRoute.get("/produtos", productsController);
